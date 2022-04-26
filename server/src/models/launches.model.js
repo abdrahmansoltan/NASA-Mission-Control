5cf3c21da5b61615baede33329726ad4dd09f4d6
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-26 19:32:41 +0200

    converted launches-database from Map to mongoDB

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,7 +1,9 @@
-const launches = new Map();
-// const launches = require("./launches.mongo");
+const launchesDatebase = require("./launches.mongo");
+const planets = require("./planets.mongo"); // to get the planets list
 
-let latestFlightNumber = 100;
+const launches = new Map();
+// let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
   flightNumber: 100,
@@ -15,30 +17,74 @@ const launch = {
 };
 
 // we use flightNumber as a key as it's Unique
-launches.set(launch.flightNumber, launch);
+// launches.set(launch.flightNumber, launch);
 
 function existsLaunchWithId(launchId) {
   console.log(launches);
   return launches.has(launchId); // true/false
 }
 
-// to make the model responsible of outputing clean and ready data
-function getAllLaunches() {
-  return Array.from(launches.values());
+async function getlatestFlightNumber() {
+  const latestLaunch = await launchesDatebase.findOne().sort("-flightNumber"); // sort by the property: "flightNumber" in descending order (-)  and returning the first one
+
+  // if there's no launches in the database
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+
+  return latestLaunch.flightNumber;
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    latestFlightNumber,
-    Object.assign(launch, {
-      success: true,
-      upcoming: true,
-      customer: ["ZTM", "NASA"],
-      flightNumber: latestFlightNumber,
-    })
+// to make the model responsible of outputing clean and ready data
+async function getAllLaunches() {
+  // return Array.from(launches.values());
+  return await launchesDatebase.find({}, { _id: 0, __v: 0 }); // find all documents without showng the id or veersion-key in the response
+}
+
+//--------------- inserting new launch----------------- //
+// using mongoDB
+async function saveLaunch(launch) {
+  // checking for integrity as if the planet's name exists in the database
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  }); // the filter says that the "launch.target" must be from the names of the planets (keplerName) from planets database
+  if (!planet) {
+    throw new Error(`No matching planet was found`);
+  }
+
+  await launchesDatebase.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    { upsert: true }
   );
 }
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = (await getlatestFlightNumber()) + 1;
+
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customer: ["ZTM", "NASA"],
+    flightNumber: newFlightNumber,
+  });
+
+  await saveLaunch(newLaunch);
+}
+
+// function addNewLaunch(launch) {
+//   latestFlightNumber++;
+//   launches.set(
+//     latestFlightNumber,
+//     Object.assign(launch, {
+//       success: true,
+//       upcoming: true,
+//       customer: ["ZTM", "NASA"],
+//       flightNumber: latestFlightNumber,
+//     })
+//   );
+// }
 
 function abortLaunchById(launchID) {
   const aborted = launches.get(launchID);
@@ -50,6 +96,7 @@ function abortLaunchById(launchID) {
 module.exports = {
   existsLaunchWithId,
   getAllLaunches,
-  addNewLaunch,
+  // addNewLaunch,
+  scheduleNewLaunch,
   abortLaunchById,
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,15 +1,16 @@
 const {
   getAllLaunches,
   addNewLaunch,
+  scheduleNewLaunch,
   existsLaunchWithId,
   abortLaunchById,
 } = require("../../models/launches.model");
 
-function httpgetAllLaunches(req, res) {
-  return res.status(200).json(getAllLaunches());
+async function httpgetAllLaunches(req, res) {
+  return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
   // data validation
@@ -33,7 +34,7 @@ function httpAddNewLaunch(req, res) {
     });
   }
 
-  addNewLaunch(launch);
+  await scheduleNewLaunch(launch);
 
   return res.status(201).json(launch);
 }
